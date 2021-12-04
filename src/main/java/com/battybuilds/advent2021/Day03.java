27d87bfebff2938d9458a32bd419856a22a3eba9
Tim Batty <tbatty7@gmail.com>
2021-12-03 23:49:33 -0500

    Day 3 - calculated gamma binary

--- src/main/java/com/battybuilds/advent2021/Day03.java
+++ src/main/java/com/battybuilds/advent2021/Day03.java
@@ -1,21 +1,69 @@
 package com.battybuilds.advent2021;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day03 {
 
     public int powerConsumptionCalculator(List<String> report) {
-        String binaryNumber = report.get(0);
-        int gamma = Integer.parseInt(binaryNumber, 2);
-        String epsilonBinary = "";
-        for (int index = 0; index < 5; index++) {
-            if (binaryNumber.charAt(index) == '1') {
-                epsilonBinary+="0";
-            } else {
-                epsilonBinary+="1";
-            }
-        }
+        String gammaBinary = calculateGammaBinary(report);
+        String epsilonBinary = calculateEpsilonBinary(gammaBinary);
+        int gamma = Integer.parseInt(gammaBinary, 2);
         int epsilon = Integer.parseInt(epsilonBinary, 2);
         return epsilon * gamma;
     }
+
+    private String calculateEpsilonBinary(String gammaBinary) {
+        String epsilonBinary = "";
+        for (int index = 0; index < 5; index++) {
+            if (gammaBinary.charAt(index) == '1') {
+                epsilonBinary += "0";
+            } else {
+                epsilonBinary += "1";
+            }
+        }
+        return epsilonBinary;
+    }
+
+    String calculateGammaBinary(List<String> report) {
+        String gammaBinary = "";
+        for (int index = 0; index < 5; index++) {
+            int finalIndex = index;
+            List<Character> firstChars =
+                    report.stream()
+                            .map(n -> n.charAt(finalIndex))
+                            .collect(Collectors.toList());
+            gammaBinary += calculateGammaBit(firstChars);
+        }
+        return gammaBinary;
+    }
+
+    private String calculateGammaBit(List<Character> firstChars) {
+        int ones = 0;
+        int zeroes = 0;
+        for (char bit : firstChars) {
+            if (bit == '1') {
+                ones++;
+            } else {
+                zeroes++;
+            }
+        }
+        return determineBit(ones, zeroes);
+    }
+
+    private String calculateFirstBit(String binaryNumber) {
+        int ones = 0;
+        int zeroes = 0;
+        if (binaryNumber.charAt(0) == '1') {
+            ones++;
+        } else {
+            zeroes++;
+        }
+        return determineBit(ones, zeroes);
+    }
+
+    private String determineBit(int ones, int zeroes) {
+        return ones > zeroes ? "1" : "0";
+    }
+
 }
--- src/test/java/com/battybuilds/advent2021/Day03Test.java
+++ src/test/java/com/battybuilds/advent2021/Day03Test.java
@@ -1,6 +1,7 @@
 package com.battybuilds.advent2021;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -9,11 +10,24 @@ import java.util.List;
 
 class Day03Test {
 
+    private Day03 diagnosticTool;
+
+    @BeforeEach
+    void setUp() {
+        diagnosticTool = new Day03();
+    }
+
     @Test
     void calculatePowerConsumptionForOneBinary() {
-        Day03 diagnosticTool = new Day03();
         List<String> report = Arrays.asList("00100");
         int powerConsumption = diagnosticTool.powerConsumptionCalculator(report);
         Assertions.assertThat(powerConsumption).isEqualTo(108);
+    }
+
+    @Test
+    void calculateGammaBinaryForThreeBinaryNumbers() {
+        List<String> report = Arrays.asList("11011", "00100", "11011");
+        String gammaBinary = diagnosticTool.calculateGammaBinary(report);
+        Assertions.assertThat(gammaBinary).isEqualTo("11011");
     }
 }
